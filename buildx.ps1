007f18d5104ab35141c03cac149f4998651d37cf
Mattia Di Eleuterio <madiele92@gmail.com>
2021-08-17 09:57:15 +0200

    Update buildx.ps1

--- buildx.ps1
+++ buildx.ps1
@@ -1,1 +1,2 @@
- docker.exe buildx build --push --platform "linux/arm/v7,linux/arm64/v8,linux/amd64,linux/386" --tag "madiele/twitch_to_podcast_rss:latest" --tag "madiele/twitch_to_podcast_rss:<VERSION> .
+# this is ste script to generate new copiled images for every architecture on windows and then push to docker.hub
+docker.exe buildx build --push --platform "linux/arm/v7,linux/arm64/v8,linux/amd64,linux/386" --tag "madiele/twitch_to_podcast_rss:latest" --tag "madiele/twitch_to_podcast_rss:<VERSION> .
