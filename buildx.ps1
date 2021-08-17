44169e0627ef62a707082104db38141967ac7ecb
Mattia Di Eleuterio <madiele92@gmail.com>
2021-08-17 20:52:47 +0200

    Update buildx.ps1

--- buildx.ps1
+++ buildx.ps1
@@ -3,4 +3,4 @@ param($version)
 if ($version -eq $null) {
 	$version = read-host -Prompt "pleas input version to push example: v1.2"
 }
-docker.exe buildx build --push --platform "linux/arm/v7,linux/arm64/v8,linux/amd64,linux/386" --tag "madiele/twitch_to_podcast_rss:latest" --tag "madiele/twitch_to_podcast_rss:$version" .
+docker.exe buildx build --push --platform "linux/arm/v7,linux/arm64/v8,linux/amd64,linux/386" --tag "madiele/twitch_to_podcast_rss:$version" --tag "madiele/twitch_to_podcast_rss:latest" .
